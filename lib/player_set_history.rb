ed4f7eec608e07a09831d34c5350283a0a8b0563
Jouse then <jthen93@gmail.com>
2021-03-15 18:38:02 +0000

    First Commit

---
+++ lib/player_set_history.rb
@@ -0,0 +1,6 @@
+require "player_set_history/version"
+
+module PlayerSetHistory
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/player_set_history/version.rb
@@ -0,0 +1,3 @@
+module PlayerSetHistory
+  VERSION = "0.1.0"
+end
