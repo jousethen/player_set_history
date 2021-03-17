8b6cdf5ee6599bd9db35ab2676bfee46b03ed18b
Jouse then <jthen93@gmail.com>
2021-03-17 17:35:58 +0000

    Created base for Player Class

---
+++ lib/player_set_history/player.rb
@@ -0,0 +1,14 @@
+class PlayerSetHistory::Player
+  attr_accessor :slug, :tag, :twitter, :discord, :twitch, :pronoun, :state, :country, :player_id
+  @@all = []
+  
+  def initiliaze(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)} 
+    @@all << self
+  end
+  
+  def add_player_attributes(attributes)
+    attributes_hash.each {|key, value| self.send(("#{key}="), value)}
+  end
+  
+end
--- player_set_history.gemspec
+++ player_set_history.gemspec
@@ -39,6 +39,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 2.0"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_dependency "pry"
-  spec.add_dependency "graphql"
   spec.add_dependency "httparty"
 end
