c7485ae9c6c38c47cb83a8387f5add282f4ed111
Jouse then <jthen93@gmail.com>
2021-03-19 15:17:39 +0000

    Got all game codes for user to decide which events to filter

--- lib/player_set_history/climanager.rb
+++ lib/player_set_history/climanager.rb
@@ -8,7 +8,14 @@ class PlayerSetHistory::CLIManager
   
   def call
     input = ""
-    puts "Welcome! Please input the player slug to continue \n\n"
+    puts "Welcome! Please Choose the game we will be pulling sets from"
+    puts "Street Fighter V: Champion Edition 33990"
+    puts "Guilty Gear Accent Core Plus R 22406"
+    puts "Guilty Gear Xrd Rev 2 36"
+    puts "Guilty Gear Strive 33945"
+    puts "Tekken 7 17"
+    puts "Smash Ultimate 1386"
+    puts "Mortal Kombat 11 3200"
     puts "To find the player slug, go to their smash.gg profile. You can pull it from the page itself or the url: \n\nSample: Smash.gg/user/[slug]"
     
     while input == "" do
