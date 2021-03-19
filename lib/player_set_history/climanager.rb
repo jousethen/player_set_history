01b79eaec5507894672485acbca409f156d05d50
Jouse then <jthen93@gmail.com>
2021-03-19 16:04:50 +0000

    Added #get_game_id to CLIManager

--- lib/player_set_history/climanager.rb
+++ lib/player_set_history/climanager.rb
@@ -8,21 +8,45 @@ class PlayerSetHistory::CLIManager
   
   def call
     input = ""
-    puts "Welcome! Please Choose the game we will be pulling sets from"
-    puts "Street Fighter V: Champion Edition 33990"
-    puts "Guilty Gear Accent Core Plus R 22406"
-    puts "Guilty Gear Xrd Rev 2 36"
-    puts "Guilty Gear Strive 33945"
-    puts "Tekken 7 17"
-    puts "Smash Ultimate 1386"
-    puts "Mortal Kombat 11 3200"
-    puts "To find the player slug, go to their smash.gg profile. You can pull it from the page itself or the url: \n\nSample: Smash.gg/user/[slug]"
+    puts "Welcome! Please Choose the number game we will be pulling sets from"
+    puts "1) Street Fighter V: Champion Edition 33990"
+    puts "2) Guilty Gear Accent Core Plus R 22406"
+    puts "3) Guilty Gear Xrd Rev 2 36"
+    puts "4) Guilty Gear Strive 33945"
+    puts "5) Tekken 7 17"
+    puts "6) Smash Ultimate 1386"
+    puts "7) Mortal Kombat 11 3200"
     
-    while input == "" do
+    
+    
+    until input.to_i.between?(1,7) do
+      puts "The input must be a valid number"
       input = gets.chomp
     end
     
+    
+    puts "To find the player slug, go to their smash.gg profile. You can pull it from the page itself or the url: \n\nSample: Smash.gg/user/[slug]"
   end
   
   
+  def get_game_id (num)
+    case input
+      when "1"
+        return "33990"
+      when "2"
+        return "22406"
+      when "3"
+        return "36"
+      when "4"
+        return "33945"
+      when "5"
+        return "17"
+      when "6"
+        return "1386"
+      when "7"
+        return "3200"
+      end
+  end
+  
 end
+
